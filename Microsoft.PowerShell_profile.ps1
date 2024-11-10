4fe025ec9d8dafcf79a521644807417ca3bb42c5
AjayN <ajaynarayanan11@gmail.com>
2024-11-10 17:02:03 +0530

    Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+set-Alias vim nvim
+
+set-Alias rvi nvim
+
+
+#oh-my-posh init pwsh | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/clean-detailed.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/paradox.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/blueish.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/gruvbox.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/if_tea.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/powerline.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/xtoys.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/tonybaloney.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/takuya.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/sonicboom_light.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/sonicboom_dark.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/slimfat.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/fish.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/aliens.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/agnoster.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/M365Princess.omp.json' | Invoke-Expression
+
+#oh-my-posh init pwsh --config 'C:/Users/ajayn/OneDrive/Documents/PowerShell/dracula.omp.json' | Invoke-Expression
+
+oh-my-posh init pwsh --config '~/OneDrive/Documents/PowerShell/Themes/jblab_2021.omp.json' | Invoke-Expression
+
+
+
+
